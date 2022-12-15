deabf64675154a172e57721659a363d838d053de
Gfcop <kharearchit121@gmail.com>
2022-12-15 15:11:31 +0530

    Update config.py

--- config.py
+++ config.py
@@ -10,11 +10,11 @@ class Config(object):
     log = True
     APP_ID = getenv("API_ID", "6435225")
     API_HASH = getenv("API_HASH", "4e984ea35f854762dcde906dce426c2d")
-    TOKEN = getenv("TOKEN", "5887815440:AAF9j0Dqjhel9vMbCwo5xJmhg-uFRzvWD94")
+    TOKEN = getenv("TOKEN", "5887815440:AAF95gTRiUm0Dl81zOazBBLaF0hO01xb0dc")
     OWNER_ID = getenv("OWNER_ID", "5540577046")
     STRING_SESSION = getenv("STRING_SESSION", "1AZWarzwBu3xk1G65wgK4zw6o54z9rvz7WdMk5sAYKu6zBMzhrLn0rqJ_9ei9CDH6Zn1gSWJ41L5W9CGqLFix2zE0G4lfyJ-liT94CYU71CrCgiUjzly0LkNKPJ7Uk9Uq1RmJzBVorKk4LNCtFTN6ZngqyBuvF9A8j4ivLHo8_wKLP-8iAEeZ2zvQrGbzVP6YNqbNeL516nDCE2wkj5DmmyI9WW0DBy_RBvJDiilXZV0rnpZBIfH15q5JibNhop32g-nywZtMM03XzAnH04O6oGKgG8HwzqZopFcwzCljM9ucjEWS2cHkCOBV4dphow98s-0i1Rn5qLRmr3xvusggBicmko73CjQ=")
     OWNER_USERNAME = getenv("OWNER_USERNAME", "notaakash")
-    DB_URI = getenv("DATABASE_URL", "postgres://aobtgusl:B8tX04QyWl6QFtLwMI4GxBbi-1xqQz7X@mel.db.elephantsql.com/aobtgusl")
+    DB_URI = getenv("DATABASE_URL", "postgres://srwrinik:Xg_jWUxCx1bLsIDLBlxlun4C_-NemfcE@mel.db.elephantsql.com/srwrinik")
     DB_URI = DB_URI.replace("postgres", "postgresql")
     MESSAGE_DUMP = getenv("MESSAGE_DUMP", "-1001851677864")
     GBAN_LOGS = getenv("GBAN_LOGS", "-1001851677864")
