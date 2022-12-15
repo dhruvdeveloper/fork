589c3941c68afbef58e2f2fa16f91f25a4824962
Gfcop <kharearchit121@gmail.com>
2022-12-15 13:31:15 +0530

    Update Logo2.py

--- Telegram/modules/Logo2.py
+++ Telegram/modules/Logo2.py
@@ -113,7 +113,7 @@ async def logo_gen(event):
         await tbot.send_file(
             event.chat_id,
             file=flnme,
-            caption="Logo by [@SUPERIOR_BOTS](https://t.me/SUPERIOR_BOTS)",
+            caption="Logo by [@WILDUPDATES](https://t.me/WILDUPDATES)",
             force_document=False,
         )
         os.remove(flnme)
@@ -196,7 +196,7 @@ async def logo_(event):
         await tbot.send_file(
             event.chat_id,
             file=flnme,
-            caption="Logo by [@SUPERIOR_BOTS](https://t.me/SUPERIOR_BOTS)",
+            caption="Logo by [@WILDUPDATES](https://t.me/WILDUPDATES)",
             force_document=False,
         )
         os.remove(flnme)
